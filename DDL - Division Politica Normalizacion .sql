91a7e5519818c5e188a677744bfc9ea017403399
andres espejo <jefferson.espejo@gmail.com>
2024-04-07 23:07:39 -0500

    Juntar script original con normalizacion

--- DDL - Division Politica Normalizacion .sql
+++ DDL - Division Politica Normalizacion .sql
@@ -1,25 +1,25 @@
 --Ejecutar primero
 DROP DATABASE DivisionPolitica WITH (FORCE);
 --Ejecutar segundo
-CREATE DATABASE DivisionPolitica; 
+CREATE DATABASE DivisionPolitica;
 
 /*
 -- Consulta para conocer la estructura de una tabla
 SELECT * FROM INFORMATION_SCHEMA.COLUMNS
 	WHERE TABLE_NAME='Pais'
 
--- Consulta para conocer la estructura de los indices de una tabla	
-SELECT sys.indexes.name AS Indice, 
+-- Consulta para conocer la estructura de los indices de una tabla
+SELECT sys.indexes.name AS Indice,
 	INFORMATION_SCHEMA.COLUMNS.COLUMN_NAME AS Campo,
-	sys.indexes.is_primary_key 
-	FROM INFORMATION_SCHEMA.COLUMNS INNER JOIN sys.indexes 
-			ON OBJECT_ID(TABLE_NAME) = [object_id] 
-		INNER JOIN sys.index_columns 
+	sys.indexes.is_primary_key
+	FROM INFORMATION_SCHEMA.COLUMNS INNER JOIN sys.indexes
+			ON OBJECT_ID(TABLE_NAME) = [object_id]
+		INNER JOIN sys.index_columns
 			ON sys.indexes.index_id = sys.index_columns.index_id
 			AND sys.indexes.[object_id] = sys.index_columns.[object_id]
-			AND ORDINAL_POSITION=column_id 
-	WHERE sys.indexes.[object_id] = OBJECT_ID('Ciudad') 
-	ORDER BY sys.indexes.name, 
+			AND ORDINAL_POSITION=column_id
+	WHERE sys.indexes.[object_id] = OBJECT_ID('Ciudad')
+	ORDER BY sys.indexes.name,
 		sys.index_columns.index_column_id,
 		INFORMATION_SCHEMA.COLUMNS.COLUMN_NAME
 */
@@ -27,7 +27,7 @@ SELECT sys.indexes.name AS Indice,
 --Para las siguientes instrucciones, se debe cambiar la conexión
 
 -- Crear tabla CONTINENTE
-CREATE TABLE Continente( 
+CREATE TABLE Continente(
 	Id SERIAL PRIMARY KEY,
 	Nombre VARCHAR(50) NOT NULL
 );
@@ -37,7 +37,7 @@ CREATE UNIQUE INDEX ixContinente_Nombre
 	ON Continente(Nombre);
 
 -- Crear tabla TIPOREGION
-CREATE TABLE TipoRegion( 
+CREATE TABLE TipoRegion(
 	Id SERIAL PRIMARY KEY,
 	TipoRegion VARCHAR(50) NOT NULL
 );
@@ -46,11 +46,11 @@ CREATE TABLE TipoRegion(
 CREATE UNIQUE INDEX ixTipoRegion_TipoRegion
 	ON TipoRegion(TipoRegion);
 
--- Crear tabla PAIS 
-CREATE TABLE Pais( 
+-- Crear tabla PAIS
+CREATE TABLE Pais(
 	Id SERIAL PRIMARY KEY,
-	Nombre VARCHAR(50) NOT NULL, 
-	IdContinente INTEGER NOT NULL, 
+	Nombre VARCHAR(50) NOT NULL,
+	IdContinente INTEGER NOT NULL,
 	CONSTRAINT fkPais_IdContinente FOREIGN KEY (IdContinente)
 		REFERENCES Continente(Id),
 	IdTipoRegion INTEGER NOT NULL,
@@ -64,38 +64,38 @@ CREATE UNIQUE INDEX ixPais_Nombre
 	ON Pais(Nombre);
 
 -- Crear tabla REGION
-CREATE TABLE Region( 
+CREATE TABLE Region(
 	Id SERIAL PRIMARY KEY,
-	Nombre VARCHAR(50) NOT NULL, 
-	IdPais INTEGER NOT NULL, 
+	Nombre VARCHAR(50) NOT NULL,
+	IdPais INTEGER NOT NULL,
 	CONSTRAINT fkRegion_IdPais FOREIGN KEY (IdPais)
 		REFERENCES Pais(Id),
-	Area FLOAT NULL, 
+	Area FLOAT NULL,
 	Poblacion INTEGER NULL
 );
 
 -- Crear indice para REGION	ordenado por PAIS y NOMBRE
 CREATE UNIQUE INDEX ixRegion_IdPais_Nombre
 	ON Region(IdPais,Nombre);
-    
+
 -- Crear tabla CIUDAD
-CREATE TABLE Ciudad( 
+CREATE TABLE Ciudad(
 	Id SERIAL PRIMARY KEY,
-	Nombre VARCHAR(50) NOT NULL, 
-	IdRegion INTEGER NOT NULL, 
+	Nombre VARCHAR(50) NOT NULL,
+	IdRegion INTEGER NOT NULL,
 	CONSTRAINT fkCiudad_IdRegion FOREIGN KEY (IdRegion)
 		REFERENCES Region(Id),
-	Area FLOAT NULL, 
+	Area FLOAT NULL,
 	Poblacion INTEGER NULL,
 	CapitalPais BOOLEAN DEFAULT false NOT NULL,
 	CapitalRegion BOOLEAN DEFAULT false NOT NULL,
 	AreaMetropolitana BOOLEAN DEFAULT false NOT NULL
 );
 
--- Crear indice para CIUDAD	ordenado por REGION y NOMBRE 
+-- Crear indice para CIUDAD	ordenado por REGION y NOMBRE
 CREATE UNIQUE INDEX ixCiudad_IdRegion_Nombre
 	ON Ciudad(IdRegion,Nombre);
-	
+
 --	Creacion de Vista de Paises, Regiones y Ciudades
 CREATE VIEW vwCiudades AS
 	SELECT C.Id IdCiudad, C.Nombre Ciudad,
@@ -105,4 +105,45 @@ CREATE VIEW vwCiudades AS
 	FROM Pais P
 		LEFT JOIN Region R ON R.IdPais=P.Id
 		LEFT JOIN Ciudad C ON C.IdRegion = R.Id;
+
+-- Aqui empieza la normalizacion
+-- Crear tabla moneda
+CREATE TABLE Moneda (
+    Id SERIAL PRIMARY KEY,
+    Moneda VARCHAR(100) NOT NULL,
+    Sigla VARCHAR(5) NOT NULL,
+    Imagen BYTEA
+);
+
+-- Crear indice para Moneda ordenado por Moneda
+CREATE UNIQUE INDEX ixMoneda_Moneda
+	ON Moneda(Moneda);
+
+-- Agregar atributo idMoneda
+ALTER TABLE Pais
+ADD COLUMN IdMoneda INTEGER;
+
+-- Llenar la tabla moneda con las monedas que hay en pais
+INSERT INTO Moneda (Moneda, Sigla)
+SELECT DISTINCT Moneda, ''
+FROM Pais;
+
+-- Actualizar el atributo idMoneda comparando con el id de la tabla moneda
+UPDATE Pais
+SET IdMoneda = (SELECT Id FROM Moneda WHERE Moneda.Moneda = Pais.Moneda)
+WHERE Moneda IS NOT NULL;
+
+--Agregar la tabla foranea para relacionar Moneda y Pais
+ALTER TABLE Pais
+ADD CONSTRAINT fkPais_IdMoneda FOREIGN KEY (IdMoneda)
+    REFERENCES Moneda(Id);
+
+-- Agregar columnas Mapa y Bandera a la tabla pais
+ALTER TABLE pais
+ADD COLUMN Mapa BYTEA,
+ADD COLUMN Bandera BYTEA;
+
+-- Hacer Drop de la columna Moneda de la tabla pais
+ALTER TABLE Pais
+DROP COLUMN Moneda;
 
